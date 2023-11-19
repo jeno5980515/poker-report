0963c2fd710f9640d684f973885cdb9cc8557832
jeno5980515 <jeno5980515@yahoo.com.tw>
2023-11-19 23:46:49 +0800

    Add Range Page Board

--- src/pages/SolutionPage/index.js
+++ src/pages/SolutionPage/index.js
@@ -17,7 +17,17 @@ const Wrapper = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
+	flex-direction: column;
+	width: 100%;
 `
+
+const SolutionPageWrapper = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+`
+
 
 const Board = styled.div`
 	display: flex;
@@ -35,7 +45,7 @@ const HandDivWrapper = styled.div`
   aspect-ratio: 1/1;
 	display: flex;
 	align-items: center;
-	justify-content: center;
+	justify-content: ${({ isFreq }) => isFreq ? 'center' : 'start'};;
 	border: black 1px solid;
 	font-size: 0.7em;
 	user-select: none;
@@ -44,7 +54,7 @@ const HandDivWrapper = styled.div`
 
 const ColorBlock = styled.div`
 	width: ${({ width }) => width}%;
-	background: ${({ color }) => color};
+	background: ${({ color }) => color };
 	height: 100%;
 `
 
@@ -55,9 +65,10 @@ const TextBlock = styled.div`
 const Page = styled.div`
 	display: flex;
 	flex-direction: column;
+	width: 100%;
 `
 
-const Detail = styled.div`
+const StrategyDetail = styled.div`
 	width: 450px;
 	display: flex;
 	flex-direction: column;
@@ -65,10 +76,40 @@ const Detail = styled.div`
 	justify-content: space-between;
 `
 
+const RangeDetail = styled.div`
+	width: 450px;
+	display: flex;
+	flex-direction: column;
+	height: 550px;
+	justify-content: space-between;
+`
+
+
 const DetailControlWrapper = styled.div`
 	display: flex;
 	margin-top: 70px;
 `
+
+const SolutionPageControlWrapper = styled.div`
+	display: flex;
+	margin-top: 70px;
+`
+
+const Chart = styled.div`
+	width: 100%;
+	height: 300px;
+`
+
+const BoardWrapper = styled.div`
+	display: flex;
+`
+
+const RangeLeft = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+`
+
 
 const sortBySize = (a, b) => {
 	if (a[0] === 'X') {
@@ -115,7 +156,7 @@ const COLOR_MAP = {
 
 const RANGE = [
 	['AA', 'AKs', 'AQs', 'AJs', 'ATs', 'A9s', 'A8s', 'A7s', 'A6s', 'A5s', 'A4s', 'A3s', 'A2s'],
-	['AKo', 'KK', 'KQs', 'KJs', 'KTs', 'K9s', 'A8s', 'A7s', 'K6s', 'K5s', 'K4s', 'K3s', 'K2s'],
+	['AKo', 'KK', 'KQs', 'KJs', 'KTs', 'K9s', 'K8s', 'K7s', 'K6s', 'K5s', 'K4s', 'K3s', 'K2s'],
 	['AQo', 'KQo', 'QQ', 'QJs', 'QTs', 'Q9s', 'Q8s', 'Q7s', 'Q6s', 'Q5s', 'Q4s', 'Q3s', 'Q2s'],
 	['AJo', 'KJo', 'QJo', 'JJ', 'JTs', 'J9s', 'J8s', 'J7s', 'J6s', 'J5s', 'J4s', 'J3s', 'J2s'],
 	['ATo', 'KTo', 'QTo', 'JTo', 'TT', 'T9s', 'T8s', 'T7s', 'T6s', 'T5s', 'T4s', 'T3s', 'T2s'],
@@ -144,6 +185,7 @@ const HandDiv = ({
 	indexX,
 	indexY,
 	highlight,
+	isFreq = true
 }) => {
 	return <HandDivWrapper
 		onMouseEnter={onMouseEnter}
@@ -152,13 +194,16 @@ const HandDiv = ({
 		data-x={indexX}
 		data-y={indexY}
 		highlight={highlight}
+		isFreq={isFreq}
 	>
 		{
-			[...Object.entries(data.actions_total_frequencies)]
-				.sort(sortBySize)
-				.map(([key, value]) => {
-					return <ColorBlock color={COLOR_MAP[key]} width={value*100}></ColorBlock>
-				})
+			isFreq
+				? [...Object.entries(data.actions_total_frequencies)]
+					.sort(sortBySize)
+					.map(([key, value]) => {
+						return <ColorBlock color={COLOR_MAP[key]} width={value*100}></ColorBlock>
+					})
+				: <ColorBlock color={'rgb(255, 143, 0)'} width={data.total_frequency * 100}></ColorBlock>
 		}
 		<TextBlock>{hand}</TextBlock>
 	</HandDivWrapper>
@@ -173,13 +218,22 @@ const RangePage = () => {
 	const [board, setBoard] = useState('2h2d2c');
 	const [data, setData] = useState(DATA[preflop][action][board])
 	const [selectedKey, setSelectedKey] = useState('AA')
-	const [detailState, setDetailState] = useState('filters')
+	const [pageState, setPageState] = useState('range')
+	const [detailState, setDetailState] = useState('hands')
 	const [filterState, setFilterState] = useState({ type: 'none' })
+  const [chartData, setChartDate] = useState([10, 25, 18, 32, 12, 7]);
+	const [currentPlayer, setCurrentPlayer] = useState(2)
+  const chartRef = useRef();
 
-	const handData = data.players_info[1].simple_hand_counters;
-	const [rangeData, setRangeData] = useState(RANGE.map(row => {
-		return row.map(v => ({ key: v, value : handData[v].total_frequency > 0 ? 0 : -1, combo: handData[v].total_combos, highlight: true }))
+	const player1HandData = data.players_info[0].simple_hand_counters;
+	const player2HandData = data.players_info[1].simple_hand_counters;
+	const [player1RangeData, setPlayer1RangeData] = useState(RANGE.map(row => {
+		return row.map(v => ({ key: v, value : player1HandData[v].total_frequency > 0 ? 0 : -1, combo: player1HandData[v].total_combos, highlight: true }))
 	}))
+	const [player2RangeData, setPlayer2RangeData] = useState(RANGE.map(row => {
+		return row.map(v => ({ key: v, value : player2HandData[v].total_frequency > 0 ? 0 : -1, combo: player2HandData[v].total_combos, highlight: true }))
+	}))
+
 
 	const answerCheckFreq = data.solutions[0].total_frequency;
 
@@ -237,52 +291,89 @@ const RangePage = () => {
 	}
 
 	useEffect(() => {
-		let newRangeData;
+		let newPlayer1RangeData;
+		let newPlayer2RangeData;
 		const { key, type } = filterState;
 		switch (type) {
 			case 'hands': {
-				const index = (data.players_info[1].hand_categories.find(c => c.name === key) || { index: -1 }).index 
-				newRangeData = rangeData.map((row, x) => {
+				const index1 = (data.players_info[0].hand_categories.find(c => c.name === key) || { index: -1 }).index 
+				newPlayer1RangeData = player1RangeData.map((row, x) => {
 					return row.map((v, y) => {
 						return {
 							...v,
-							highlight: v.value === -1 ? false : INDEX_MAP[v.key].some(i => data.hand_categories_range[i] === index)
+							highlight: v.value === -1 ? false : INDEX_MAP[v.key].some(i => data.hand_categories_range[i] === index1)
+						}
+					})
+				})
+				const index2 = (data.players_info[1].hand_categories.find(c => c.name === key) || { index: -1 }).index 
+				newPlayer2RangeData = player2RangeData.map((row, x) => {
+					return row.map((v, y) => {
+						return {
+							...v,
+							highlight: v.value === -1 ? false : INDEX_MAP[v.key].some(i => data.hand_categories_range[i] === index2)
 						}
 					})
 				})
 				break;
 			}
 			case 'draw': {
-				const index = (data.players_info[1].draw_categories.find(c => c.name === key) || { index: -1 }).index 
-				newRangeData = rangeData.map((row, x) => {
+				const index1 = (data.players_info[0].draw_categories.find(c => c.name === key) || { index: -1 }).index 
+				newPlayer1RangeData = player1RangeData.map((row, x) => {
 					return row.map((v, y) => {
 						return {
 							...v,
-							highlight: v.value === -1 ? false : INDEX_MAP[v.key].some(i => data.draw_categories_range[i] === index)
+							highlight: v.value === -1 ? false : INDEX_MAP[v.key].some(i => data.draw_categories_range[i] === index1)
+						}
+					})
+				})
+				const index2 = (data.players_info[1].draw_categories.find(c => c.name === key) || { index: -1 }).index 
+				newPlayer2RangeData = player2RangeData.map((row, x) => {
+					return row.map((v, y) => {
+						return {
+							...v,
+							highlight: v.value === -1 ? false : INDEX_MAP[v.key].some(i => data.draw_categories_range[i] === index2)
 						}
 					})
 				})
 				break;
 			}
 			case 'eqs': {
-				const index = (data.players_info[1].equity_buckets.find(c => c.name === key) || { index: -1 }).index 
-				newRangeData = rangeData.map((row, x) => {
+				const index1 = (data.players_info[0].equity_buckets.find(c => c.name === key) || { index: -1 }).index 
+				newPlayer1RangeData = player1RangeData.map((row, x) => {
 					return row.map((v, y) => {
 						return {
 							...v,
-							highlight: v.value === -1 ? false : INDEX_MAP[v.key].some(i => data.players_info[1].equity_buckets_range[i] === index)
+							highlight: v.value === -1 ? false : INDEX_MAP[v.key].some(i => data.players_info[0].equity_buckets_range[i] === index1)
+						}
+					})
+				})
+				const index2 = (data.players_info[1].equity_buckets.find(c => c.name === key) || { index: -1 }).index 
+				newPlayer2RangeData = player2RangeData.map((row, x) => {
+					return row.map((v, y) => {
+						return {
+							...v,
+							highlight: v.value === -1 ? false : INDEX_MAP[v.key].some(i => data.players_info[1].equity_buckets_range[i] === index2)
 						}
 					})
 				})
 				break;
 			}
 			case 'eqa': {
-				const index = (data.players_info[1].equity_buckets_advanced.find(c => c.name === key) || { index: -1 }).index 
-				newRangeData = rangeData.map((row, x) => {
+				const index1 = (data.players_info[0].equity_buckets_advanced.find(c => c.name === key) || { index: -1 }).index 
+				newPlayer1RangeData = player1RangeData.map((row, x) => {
 					return row.map((v, y) => {
 						return {
 							...v,
-							highlight: v.value === -1 ? false : INDEX_MAP[v.key].some(i => data.players_info[1].equity_buckets_advanced_range[i] === index)
+							highlight: v.value === -1 ? false : INDEX_MAP[v.key].some(i => data.players_info[0].equity_buckets_advanced_range[i] === index1)
+						}
+					})
+				})
+				const index2 = (data.players_info[1].equity_buckets_advanced.find(c => c.name === key) || { index: -1 }).index 
+				newPlayer2RangeData = player2RangeData.map((row, x) => {
+					return row.map((v, y) => {
+						return {
+							...v,
+							highlight: v.value === -1 ? false : INDEX_MAP[v.key].some(i => data.players_info[1].equity_buckets_advanced_range[i] === index2)
 						}
 					})
 				})
@@ -290,7 +381,15 @@ const RangePage = () => {
 			}
 			case 'none':
 			default: {
-				newRangeData = rangeData.map((row, x) => {
+				newPlayer1RangeData = player1RangeData.map((row, x) => {
+					return row.map((v, y) => {
+						return {
+							...v,
+							highlight: true
+						}
+					})
+				})
+				newPlayer2RangeData = player2RangeData.map((row, x) => {
 					return row.map((v, y) => {
 						return {
 							...v,
@@ -300,26 +399,109 @@ const RangePage = () => {
 				})
 			}
 		}
-		setRangeData(newRangeData)
+		setPlayer1RangeData(newPlayer1RangeData)
+		setPlayer2RangeData(newPlayer2RangeData)
 	}, [JSON.stringify(filterState)])
 
+
+  useEffect(() => {
+		if (!chartRef.current) {
+			return
+		}
+
+		let player1Data = data.players_info[0].hand_eqs
+			.filter(d => d !== 0)
+			.map(d => d * 100).sort()
+		let player2Data = data.players_info[1].hand_eqs
+			.filter(d => d !== 0)
+			.map(d => d * 100).sort()
+
+		const gap = player2Data.length - player1Data.length;
+		// player1Data = Array(gap).fill(0).concat(player1Data)
+		player2Data = [...player2Data.slice(gap)]
+    const width = 500;
+    const height = 250;
+    const margin = { top: 20, right: 20, bottom: 20, left: 40 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+
+    d3.select(chartRef.current).selectAll('*').remove();
+
+    const svg = d3
+      .select(chartRef.current)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
+
+    const xScale = d3.scaleLinear().domain([0, Math.max(player1Data.length, player2Data.length) - 1]).range([0, innerWidth]);
+    const yScale1 = d3.scaleLinear().domain([0, d3.max(player1Data)]).range([innerHeight, 0]);
+    const line1 = d3.line().x((d, i) => xScale(i)).y((d) => yScale1(d));
+
+    svg
+      .append('path')
+      .datum(player1Data)
+      .attr('transform', `translate(${margin.left},${margin.top})`)
+      .attr('fill', 'none')
+      .attr('stroke', 'blue')
+      .attr('stroke', 'rgb(151, 234, 248)')
+      .attr('stroke-width', 2)
+      .attr('d', line1);
+
+    const yScale2 = d3.scaleLinear().domain([0, d3.max(player2Data)]).range([innerHeight, 0]);
+    const line2 = d3.line().x((d, i) => xScale(i)).y((d) => yScale2(d));
+
+    svg
+      .append('path')
+      .datum(player2Data)
+      .attr('transform', `translate(${margin.left},${margin.top})`)
+      .attr('fill', 'none')
+      .attr('stroke', 'red')
+      .attr('stroke', 'rgb(37, 179, 54)')
+      .attr('stroke-width', 2)
+      .attr('d', line2);
+
+    const xAxis = d3.axisBottom(d3.scaleLinear().domain([0, 100]).range([0, innerWidth])).tickValues([25,50,75,100]).tickSizeInner(-innerHeight).tickSizeOuter(0).tickPadding(-3)
+		const yAxis = d3.axisLeft(yScale1).tickValues([0,25,50,75,100])
+		yAxis.tickSize(-5)
+		xAxis.tickSize(-5)
+
+    svg
+      .append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`)
+      .call(yAxis)
+			.selectAll('text')
+			.style('fill', 'white')
+			.selectAll('.tick line')
+			.style('stroke', 'white');
+  
+    svg
+      .append('g')
+      .attr('transform', `translate(${margin.left},${innerHeight + margin.top})`)
+      .call(xAxis)
+			.selectAll('text')
+			.style('fill', 'white')
+			.selectAll('.tick line')
+			.style('stroke', 'white');
+
+  }, [JSON.stringify(data), chartRef.current]);
+
+	const playerRangeData = currentPlayer === 1 ? player1RangeData : player2RangeData
 	const currentHand = data.players_info[1].simple_hand_counters[selectedKey]
 	const DetailComp = detailState === 'hands'
 		? () => <Hand data={data} indexList={INDEX_MAP[currentHand.name]} hand={selectedKey}></Hand>
 		: () => <Filter data={data} onSelectFilter={({ type, key }) => setFilterState({ type, key })} hand={selectedKey}></Filter>
-
-	return (
-		<Page>
-			<Wrapper>
+	
+	const SolutionPage = pageState === 'strategy'
+		? () => <SolutionPageWrapper>
 				<Board>
 					{
-						rangeData.map((row, x) => {
+						playerRangeData.map((row, x) => {
 							return row.map((v, y) => {
 								return <HandDiv
 									onMouseEnter={() => onHandEnter({ key: v.key })}
 									// onMouseDown={() => onHandDown({ x, y })}
 									// onMouseUp={onHandUp}
-									data={data.players_info[1].simple_hand_counters[v.key]}
+									data={data.players_info[currentPlayer === 2 ? 1 : 0].simple_hand_counters[v.key]}
 									hand={v.key}
 									highlight={v.highlight}
 								/>
@@ -327,14 +509,68 @@ const RangePage = () => {
 						})
 					}
 				</Board>
-				<Detail>
+				<StrategyDetail>
 					<Action data={data.solutions}></Action>
 					<DetailControlWrapper>
 						<button onClick={() => setDetailState('hands')}>Hands</button>
 						<button onClick={() => setDetailState('filters')}>Filters</button>
 					</DetailControlWrapper>
 					<DetailComp />
-				</Detail>
+				</StrategyDetail>
+			</SolutionPageWrapper>
+		: () => <SolutionPageWrapper>
+				<RangeLeft>
+					<BoardWrapper>
+						<Board>
+							{
+								player1RangeData.map((row, x) => {
+									return row.map((v, y) => {
+										return <HandDiv
+											onMouseEnter={() => onHandEnter({ key: v.key })}
+											// onMouseDown={() => onHandDown({ x, y })}
+											// onMouseUp={onHandUp}
+											data={data.players_info[0].simple_hand_counters[v.key]}
+											hand={v.key}
+											highlight={v.highlight}
+											isFreq={false}
+										/>
+									})
+								})
+							}
+						</Board>
+						<Board>
+							{
+								player2RangeData.map((row, x) => {
+									return row.map((v, y) => {
+										return <HandDiv
+											onMouseEnter={() => onHandEnter({ key: v.key })}
+											// onMouseDown={() => onHandDown({ x, y })}
+											// onMouseUp={onHandUp}
+											data={data.players_info[1].simple_hand_counters[v.key]}
+											hand={v.key}
+											highlight={v.highlight}
+											isFreq={false}
+										/>
+									})
+								})
+							}
+						</Board>
+					</BoardWrapper>
+					<Chart ref={chartRef}></Chart>
+				</RangeLeft>
+				<RangeDetail>
+					<Filter data={data} onSelectFilter={({ type, key }) => setFilterState({ type, key })} hand={selectedKey}></Filter>
+				</RangeDetail>
+			</SolutionPageWrapper>
+
+	return (
+		<Page>
+			<Wrapper>
+				<SolutionPageControlWrapper>
+					<button onClick={() => setPageState('strategy')}>Strategy</button>
+					<button onClick={() => setPageState('range')}>Range</button>
+				</SolutionPageControlWrapper>
+				<SolutionPage />
 			</Wrapper>
 		</Page>
 	)
